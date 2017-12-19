01eab11b215b6dfa846b2b9ff66f09eada830181
darkmath <mgbortoletto@gmail.com>
2017-12-19 17:15:21 -0200

    Controllers

--- toasted_server/app/controllers/posts_controller.rb
+++ toasted_server/app/controllers/posts_controller.rb
@@ -1,13 +1,45 @@
 class PostsController < ApplicationController
   load_and_authorize_resource
+  before_action :set_post, only: [:show,:edit,:update,:destroy]
+
 
   def index
+    @post = Post.new
+
+    posts = current_user.posts.map do |post|
+      post
+    end
+
+    current_user.all_following.each do |user|
+      user.posts.each do |post|
+        posts << post
+      end
+    end
+
+    @posts = posts.sort_by &:created_at
   end
 
   def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.json {render json: @post, status: :created}
+        format.html {redirect_to posts_path, notice: 'Post was successfully created.'}
+      else
+        format.json {render json: @post.errors, status: :unprocessable_entity}
+        format.html {redirect_to posts_path }
+      end
+
+    end
   end
 
   def destroy
+    @post.destroy
+    respond_to do |format|
+      format.json {head :no_content}
+      format.html {redirect_to posts_path, notice: 'Post was sucessfully destroyed.'}
+    end
   end
 
   def edit
@@ -15,4 +47,20 @@ class PostsController < ApplicationController
 
   def update
   end
+
+  def show
+    @comment = Comment.new
+    @comments = @post.comments
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:body).merge(user: current_user)
+  end
+
 end
--- toasted_server/app/controllers/users_controller.rb
+++ toasted_server/app/controllers/users_controller.rb
@@ -1,15 +1,30 @@
 class UsersController < ApplicationController
   load_and_authorize_resource
+  before_action :set_user, only: [:show, :follwing, :followers]
 
   def show
+    @posts = @user.posts
+    @following = @user.following_users.limit(5)
+    @followers = @user.followers_by_type('User').limit(5)
   end
 
   def followers
+    @followers = @user.following_users
   end
 
-  def followings
+  def following
+    @following = @user.following_users
   end
 
   def search
+    @user = current_user
+    @query = User.ransack(params[:q])
+    @user = @query.result(distinct: true)
   end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
